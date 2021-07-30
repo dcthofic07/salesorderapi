5cd03b1d169c17ea025027ee8a2b2e4cc4e4a2a0
Thofic <65671479+dcthofic07@users.noreply.github.com>
2021-07-30 15:17:07 +0100

    Update getdatafromxml.py

--- getdatafromxml.py
+++ getdatafromxml.py
@@ -6,7 +6,7 @@ class TestMatchSalesOrder(unittest.TestCase):
 
     def test_b(self):
 
-        open_xml = open("./salesorderapi/SB S21 Sales XML2021062313 24 17.xml", "r")
+        open_xml = open("SB S21 Sales XML2021062313 24 17.xml", "r")
         so = GetSalesOrder.TestGetSalesOrder.test_readdata(self)
         flag = 0
         index = 0
